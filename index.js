336c99e191ce780f28b20a30c9b18165eb46bf28
Carlie Duperval <ritchellcarlie@gmail.com>
2022-07-19 08:14:38 -0400

    Converter us dollar

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ function convertUstoGourde (usdConverter, rateConverter){
 
 const newValue = convertUstoGourde(uds, haitianGourde)
 console.log("There are " , uds, "UsD in", newValue + " Gourdes")
+
+
+
+const convertUsDollar = ( us, rate) => {
+ let result = us * rate
+ return result
+}
+const value = convertUsDollar(3, 119)
+console.log(`There is 3$ in `, value,`Gourde`)
+console.log(`There is $ in gourdes `, convertUsDollar(15, 108))
